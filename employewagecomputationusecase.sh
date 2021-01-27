318fbdbc8e334f2b1787a2cd1cf9e1689979edb3
Monika <monikadkale98@gmail.com>
2021-01-27 14:39:34 +0530

    added  new line code

--- employewagecomputationusecase.sh
+++ employewagecomputationusecase.sh
@@ -4,12 +4,17 @@ echo "...................................Welcome to Employee Wage Computation...
 
 #CONSTANT
 IS_PRESENT=1
+WAGE_PER_HOUR=20
 
 #CHECK EMPLOYEE IS PRESENT OR ABSENT
 randomNumber=$((RANDOM%2))
 if [ $randomNumber -eq $IS_PRESENT ]
 then
 	echo "Employee is Present"
+	employeeHour=8
 else
 	echo "Employee is Absent"
+	employeeHour=0
 fi
+salary=$(($employeeHour*$WAGE_PER_HOUR))
+echo "Salary =" $salary
